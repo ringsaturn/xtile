f8dffa672f778085f4f4faca3117b2da60782acf
ringsaturn <ringsaturn.me@gmail.com>
2022-09-16 23:36:52 +0800

    init xtile

---
+++ xtile.go
@@ -0,0 +1,19 @@
+package xtile
+
+import "math"
+
+// ZoomForBounds copy from
+// https://stackoverflow.com/questions/4266754/how-to-calculate-google-maps-zoom-level-for-a-bounding-box-in-java
+func ZoomForBounds(lngWest float64, latSouth float64, lngEat float64, latNorth float64) int {
+	latDiff := math.Abs(latNorth - latSouth)
+	lngDiff := math.Abs(lngWest - lngEat)
+	maxDiff := math.Max(latDiff, lngDiff)
+	if maxDiff < 360/math.Pow(2, 20) {
+		return 21
+	}
+	zoom := int((-1 * ((math.Log(maxDiff) / math.Log(2)) - (math.Log(360) / math.Log(2)))))
+	if zoom < 1 {
+		zoom = 1
+	}
+	return zoom
+}
